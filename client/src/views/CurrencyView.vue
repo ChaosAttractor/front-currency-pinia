3382fee1d6194a1b10c01e9042ac493c16324660
surf punk <povy.901@gmail.com>
2023-06-05 17:47:43 +0300

    axios, создание таблицы с курсами валют

--- client/src/views/CurrencyView.vue
+++ client/src/views/CurrencyView.vue
@@ -1,15 +1,41 @@
 <template>
-  <hello-world />
+  <div>
+    <v-simple-table fixed-header height="900px">
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th v-for="header in headers" :key="header" class="text-center">
+              {{ header }}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in getterCurrency" :key="item.NumCode">
+            <td>{{ item.NumCode }}</td>
+            <td>{{ item.CharCode }}</td>
+            <td>{{ item.Nominal }}</td>
+            <td>{{ item.Name }}</td>
+            <td class="flex flex-center gap-5px">
+              {{ item.Value }}
+              <StonksIcon :value="item.Value" :previous="item.Previous" />
+            </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
-
+console.log(process.env.VUE_APP_API_URL);
 export default {
-  name: "HomeView",
+  name: "CurrencyView",
 
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      headers: ["Цифр. код", "Бук. код", "Единиц", "Валюта", "Курс"],
+    };
   },
 };
 </script>
