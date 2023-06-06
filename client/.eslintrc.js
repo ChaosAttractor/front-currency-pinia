de2e19959ff31d3bd1613670aef8b374f78773c0
surf punk <povy.901@gmail.com>
2023-06-06 12:40:51 +0300

    eslint - настроил на options api

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   },
   rules: {
     "vue/multi-word-component-names": "off",
+    "vue/component-api-style": ["error", ["options"]],
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
